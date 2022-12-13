93250000cce49aa47a7cbcffba13ff41568ee849
DaianeM <daianersm2@gmail.com>
2022-12-12 23:11:03 -0300

    Configurando projeto Node com Nestjs, Prisma, SQLite

---
+++ src/app.controller.ts
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getNotifications() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async createNotification(@Body() body: CreateNotificationBody) {
+    // console.log('BODY', body)
+    const { recipientId, content, category } = body
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
